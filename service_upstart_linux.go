dce7d9e3c25f02535b98b51edb5dd426b33c2d71
Rafal Jeczalik <rjeczalik@gmail.com>
2016-04-20 22:37:06 +0200

    upstart: add support for Environment

--- service_upstart_linux.go
+++ service_upstart_linux.go
@@ -161,21 +161,29 @@ const upstartScript = `# {{.Description}}
 kill signal INT
 {{if .ChRoot}}chroot {{.ChRoot}}{{end}}
 {{if .WorkingDirectory}}chdir {{.WorkingDirectory}}{{end}}
-start on filesystem or runlevel [2345]
+
+start on (local-filesystems
+          and net-device-up IFACE!=lo
+          and runlevel [2345])
 stop on runlevel [!2345]
 
 {{if .UserName}}setuid {{.UserName}}{{end}}
+
+{{range $key, $value := .Environment}}
+env {{$key}}=${{value}}
+{{end}}
+
+#setuid username
 
 respawn
 respawn limit 10 5
 umask 022
 
-console none
-
 pre-start script
     test -x {{.Path}} || { stop; exit 0; }
 end script
 
-# Start
-exec {{.Path}}{{range .Arguments}} {{.|cmd}}{{end}}
+script
+    {{.Path}}{{range .Arguments}} {{.|cmd}} {{end}}
+end-script
 `
